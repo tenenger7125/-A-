575932ffb616762d19b3cbb4a50571809ac0bb82
LEE DONG GYU <tenenger7125@gmail.com>
2026-05-22 18:41:48 +0900

    feat: 임시 저장 기능 추가

--- store/enrollment-form-store/store.ts
+++ store/enrollment-form-store/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { EnrollmentStep, EnrollmentApplicationType } from './constant';
 import { EnrollmentFormStoreAction, EnrollmentFormStoreState } from './type';
 
@@ -17,12 +18,20 @@ const defaultForm: EnrollmentFormStoreState['form'] = {
   agreedToTerms: false,
 };
 
-export const enrollmentFormStore = create<EnrollmentFormStoreState & EnrollmentFormStoreAction>(set => ({
-  currentStep: defaultStep,
-  form: defaultForm,
+export const enrollmentFormStore = create<EnrollmentFormStoreState & EnrollmentFormStoreAction>()(
+  persist(
+    set => ({
+      currentStep: defaultStep,
+      form: defaultForm,
 
-  setStep: currentStep => set({ currentStep }),
-  setForm: form => set(state => ({ form: { ...state.form, ...form } })),
-  resetStep: () => set({ currentStep: defaultStep }),
-  resetForm: () => set({ form: defaultForm }),
-}));
+      setStep: currentStep => set({ currentStep }),
+      setForm: form => set(state => ({ form: { ...state.form, ...form } })),
+      resetStep: () => set({ currentStep: defaultStep }),
+      resetForm: () => set({ form: defaultForm }),
+    }),
+    {
+      name: 'enrollment-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+);
